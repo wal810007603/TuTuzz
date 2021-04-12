c750dd1d82b16396312fffdb79070f8b40395f11
wal810007603 <71761705+wal810007603@users.noreply.github.com>
2021-04-13 00:02:58 +0800

    Update and rename week4/UserDriver.java to 第4周/UserDriver.java

--- 第4周/UserDriver.java
+++ 第4周/UserDriver.java
@@ -3,16 +3,16 @@ package week4;
 public class UserDriver {
 
 	public static void main(String[] args) {
-		User u1 = new User("ÕÅÈý");
-		User u2 = new User("ÀîËÄ", "lisi444");
-		User u3 = new User("ÍõÎå", "wangwu555");
+		User u1 = new User("张三");
+		User u2 = new User("李四", "lisi444");
+		User u3 = new User("王五", "wangwu555");
 
 		u1.setPassword("zhangsan333");
 
 		System.out.println(u1.info());
 		System.out.println(u2.info());
 		System.out.println(u3.info());
-		System.out.println("ÓÃ»§¸öÊý:" + User.userNum());
+		System.out.println("人物信息数量:" + User.userNum());
 
 	}
 
