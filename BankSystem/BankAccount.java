5bfb37651af306acf63eb35dd2c623d96bcadef5
wal810007603 <71761705+wal810007603@users.noreply.github.com>
2021-04-13 00:09:23 +0800

    Create BankAccount.java

---
+++ BankSystem/BankAccount.java
@@ -0,0 +1,22 @@
+package Bank;
+
+
+public  abstract class BankAccount {
+	protected String accountNum;
+	protected double balance = 0;
+	
+	public String getAccountNum() {
+		return accountNum;
+	}
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+}
