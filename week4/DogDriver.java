f6d851bce2702c265ab6c42780c4a89a15550246
wal810007603 <71761705+wal810007603@users.noreply.github.com>
2021-04-12 23:51:13 +0800

    Create DogDriver.java

---
+++ week4/DogDriver.java
@@ -0,0 +1,14 @@
+package week4;
+
+public class DogDriver {
+
+	public static void main(String[] args) {
+
+		Dog d1 = new Dog("Ð¡ºÚ", "ºÚÉ«", 2);
+		Dog d2 = new Dog("´ó»Æ", "»ÆÉ«", 3);
+
+		System.out.println(d1.info());
+		System.out.println(d2.info());
+
+	}
+}
