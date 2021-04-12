1fe58966b474a5918a5807731cdffbf89813ed3e
wal810007603 <71761705+wal810007603@users.noreply.github.com>
2021-04-12 23:52:20 +0800

    Create EmployeeDriver.java

---
+++ 第4周/EmployeeDriver.java
@@ -0,0 +1,19 @@
+package week4;
+
+public class EmployeeDriver {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("003", "ÕÅÈý", 3000f);
+		Employee e2 = new Employee("004", "ÀîËÄ", 4000f);
+		Employee e3 = new Employee("005", "ÍõÎå", 5000f);
+
+		e1.updateSalary();
+		e2.updateSalary();
+		e3.updateSalary();
+
+		System.out.println(e1.detail());
+		System.out.println(e2.detail());
+		System.out.println(e3.detail());
+	}
+
+}
